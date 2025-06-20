a157b32845d63608e82e90ef9f927311a15c67ef
nengwi <nengmilaningrum@gmail.com>
2025-06-20 09:56:24 +0700

    akhiran dari head yg di ambil hanya div & body

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="vendor/admin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="vendor/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="vendor/admin/dist/js/adminlte.min.js"></script>
+</body>
+</html>
