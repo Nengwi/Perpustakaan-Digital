3bf285bbf94ab6f366f084f38c7a9a76e245dc6a
nengwi <nengmilaningrum@gmail.com>
2025-06-23 10:38:53 +0700

    menambahkan tabel email

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,9 +1,26 @@
-<div class="container-fluid mt-2">
+<div class="container-fluid pt-2">
   <div class="row">
     <div class="col-md-12">
       <div class="card">
         <div class="card-body">
+          <a href="" class="btn btn-primary"><i class="fas fa-plus"></i>Tambah</a>
 
+          <table class="table">
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Nengmilaningrum</td>
+              <td>nengmilaningrum@gmail.com</td>
+              <td>
+                <a href="" class="btn btn-info btn-sm"><i class="fas fa-edit"></i></a>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
